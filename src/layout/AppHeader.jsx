9000c86aecb2ceb6c52d93af3c05976cd362d108
Temirlan <temirlan-devpro@gmail.com>
2026-03-06 20:43:39 +0600

    feat(seo): add meta tags, OG, JSON-LD, fix duplicate title

--- src/layout/AppHeader.jsx
+++ src/layout/AppHeader.jsx
@@ -1,14 +1,10 @@
 import { Input, Layout } from 'antd'
-
 const { Header } = Layout
-
 const AppHeader = ({ setCity }) => {
   return (
     <Header
       className="fixed top-0 left-67.5 w-[calc(100%-270px)]
-                 h-18 z-100 bg-white! p-3 flex items-center justify-center
-                 border-b border-gray-300
-                "
+       h-18 z-100 bg-white! p-3 flex items-center justify-center border-b border-gray-300 "
     >
       <Input.Search
         placeholder="Введите город"
@@ -21,5 +17,4 @@ const AppHeader = ({ setCity }) => {
     </Header>
   )
 }
-
 export default AppHeader
